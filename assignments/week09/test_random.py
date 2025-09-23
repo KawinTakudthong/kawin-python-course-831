b285fb5711cd9f04bc23fba2427e478783d89b5b
Kawin Takudthong <kawi.t@ku.th>
2025-09-23 14:58:47 +0700

    week09

--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -5,3 +5,19 @@ def test_random():
     print(random_number)
     
 test_random()
+
+import random
+
+def test_random():
+    #สร้าง random number สุ่มระหว่าง 1-10
+    random_number = random.randint(1, 10)
+    print(random_number)
+    guess_number = input("What is your guess number : ")
+    guess_number = int(guess_number)
+    if random_number == guess_number:
+        print("Congrulations!")
+    
+    if random_number < guess_number :
+        print("Too low")
+    
+test_random()
